04ccfe327fdf216ae7bc3724f257b3af0d4f4170
Nam Le <lquocnam@gmail.com>
2013-08-22 15:20:18 +0700

    Add showAlert.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function() {
         var self = this;
         this.detailsURL = /^#contacts\/(\d{1,})/;
         this.registerEvents();
-        this.store = new MemoryStore(function() {
+        this.store = new MemoryStore(function () {
+            self.showAlert("Store Initialized", "Info");
             self.route();
         });        
     },
