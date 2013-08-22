a1c0663002660dc4c33fc9d55401d577a833a81a
Nam Le <lquocnam@gmail.com>
2013-08-22 16:57:58 +0700

    Convert to simple project structure.

--- gen/net/phonegap/excellentcompany/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.phonegap.excellentcompany;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ js/storage/device-contact-store.js
@@ -0,0 +1,24 @@
+﻿var DeviceContactStore = function (successCallback, erorrCallback) {
+    this.findByName = function (searchKey, callback) {
+        if (!navigator.contacts) {
+            return;
+        }
+        var employees = {};
+        var options = new ContactFindOptions();
+        self.showAlert("Device Contact Store", "Info");
+
+        this.callLater(callback, employees);
+    };
+
+    // Used to simulate async calls. This is done to provide a consistent interface with stores (like WebSqlStore)
+    // that use async data access APIs
+    this.callLater = function (callback, data) {
+        if (callback) {
+            setTimeout(function () {
+                callback(data);
+            });
+        }
+    };
+
+    this.callLater(successCallback);
+}
