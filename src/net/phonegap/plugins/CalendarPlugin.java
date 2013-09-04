37ff6ae4db61cd558378341afe6c9f869e658514
Nam Le <lquocnam@gmail.com>
2013-09-04 11:00:33 +0700

    Commit code for all 4 functions: View Calendar, Create Event, View Contacts, Create Contact.

--- src/net/phonegap/plugins/CalendarPlugin.java
+++ src/net/phonegap/plugins/CalendarPlugin.java
@@ -1,19 +1,32 @@
 package net.phonegap.plugins;
 
+import java.util.Date;
+
 import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
 
 
+
+@SuppressLint("NewApi")
 public class CalendarPlugin extends CordovaPlugin
 {
 	public static final String ACTION_ADD_TO_CALENDAR = "addToCalendar";
+	public static final String ACTION_ADD_TO_CONTACT = "addToContact";
+	public static final String ACTION_VIEW_CONTACT = "viewContactList";
+	public static final String ACTION_VIEW_CALENDAR = "viewCalendarEvent";
 	public static final Integer RESULT_CODE_CREATE = 0;
+	public static final Integer PICK_CONTACT = 1;
 	private CallbackContext callback;
 
 	@Override
@@ -21,18 +34,68 @@ public class CalendarPlugin extends CordovaPlugin
 	{
 		try
 		{
+			final JSONObject arg_object = args.getJSONObject(0);
+			callback = callbackContext;
 			if (ACTION_ADD_TO_CALENDAR.equals(action))
 			{
-				final JSONObject arg_object = args.getJSONObject(0);
-				callback = callbackContext;
+
+				//ACTION_EDIT
 				final Intent calIntent = new Intent(Intent.ACTION_EDIT).setType("vnd.android.cursor.item/event")
 						.putExtra("beginTime", arg_object.getLong("startTimeMillis"))
 						.putExtra("endTime", arg_object.getLong("endTimeMillis")).putExtra("title", arg_object.getString("title"))
 						.putExtra("description", arg_object.getString("description"))
 						.putExtra("eventLocation", arg_object.getString("eventLocation"));
 
+				//this.cordova.startActivityForResult(this, intent, RESULT_CODE_CREATE);
 				this.cordova.startActivityForResult(this, calIntent, RESULT_CODE_CREATE);
+
 				return true;
+
+			}
+			else if (ACTION_VIEW_CALENDAR.equals(action))
+			{
+
+				final Date now = new Date();
+
+				final long startMillis = now.getTime();
+
+				final Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+				builder.appendPath("time");
+				ContentUris.appendId(builder, startMillis);
+				final Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+
+				//this.cordova.startActivityForResult(this, intent, RESULT_CODE_CREATE);
+				this.cordova.startActivityForResult(this, intent, RESULT_CODE_CREATE);
+
+				return true;
+
+			}
+			else if (ACTION_ADD_TO_CONTACT.equals(action))
+			{
+
+				final Intent contIntent = new Intent(Intent.ACTION_INSERT, ContactsContract.Contacts.CONTENT_URI);
+				contIntent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+
+				//this.cordova.startActivityForResult(this, intent, RESULT_CODE_CREATE);
+				this.cordova.startActivityForResult(this, contIntent, PICK_CONTACT);
+
+				return true;
+
+			}
+			else if (ACTION_VIEW_CONTACT.equals(action))
+			{
+
+				final Intent contIntent = new Intent(Intent.ACTION_VIEW);
+				contIntent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+
+				//				final Intent intent = new Intent(Intent.ACTION_INSERT, ContactsContract.Contacts.CONTENT_URI);
+				//				contIntent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+
+				//this.cordova.startActivityForResult(this, intent, RESULT_CODE_CREATE);
+				this.cordova.startActivityForResult(this, contIntent, RESULT_CODE_CREATE);
+
+				return true;
+
 			}
 		}
 		catch (final Exception e)
@@ -43,6 +106,7 @@ public class CalendarPlugin extends CordovaPlugin
 
 		return false;
 	}
+
 
 	@Override
 	public void onActivityResult(final int requestCode, final int resultCode, final Intent data)
@@ -59,4 +123,5 @@ public class CalendarPlugin extends CordovaPlugin
 			}
 		}
 	}
+
 }
